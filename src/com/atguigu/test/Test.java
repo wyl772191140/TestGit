e8bda30ac4f6bf1470770d24240a56937d602965
tom_glb <goodMorning_glb@atguigu.com>
2020-11-09 15:32:29 +0800

    次分支提交，测试提交中文乱码

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("77777777777777777");
         System.out.println("888888888");
-        System.out.println("次分支提交");
+        System.out.println("次分支提交，测试提交中文乱码");
         System.out.println(System.currentTimeMillis());
     }
 }
