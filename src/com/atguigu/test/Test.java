1a5f423d181f84b03f7bbb5564042c019dd74aab
tom_glb <goodMorning_glb@atguigu.com>
2020-11-09 15:19:39 +0800

    clone.....

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("77777777777777777");
         System.out.println("888888888");
+        System.out.println(System.currentTimeMillis());
     }
 }
