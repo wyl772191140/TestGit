4a56cb337d1cb59454fb0af2da58f9833e83a325
tom_glb <goodMorning_glb@atguigu.com>
2020-11-09 15:14:26 +0800

    idea test git update

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("77777777777777777");
+        System.out.println("888888888");
     }
 }
